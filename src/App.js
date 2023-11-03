5530dc8013f491e21b9d256857182f1142e1b572
ericm <ericmage2001@gmail.com>
2023-11-03 10:25:27 -0600

    Design done, Firebase communication pending

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useRef } from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="base-div">
+      <div className="apple">
+        <h1>Apple</h1>
+        <h1>2</h1>
+        <div className="fruit-container">
+          <img
+            src={process.env.PUBLIC_URL + "/imgs/apple.png"}
+            alt="Logo"
+            className="img-fruit"
+          />
+        </div>
+      </div>
+      <div className="banana">
+        <h1>Banana</h1>
+        <h1>3</h1>
+        <div className="fruit-container">
+          <img
+            src={process.env.PUBLIC_URL + "/imgs/banana.png"}
+            alt="Logo"
+            className="img-fruit"
+          />
+        </div>
+      </div>
+      <div className="carrot">
+        <h1>Carrot</h1>
+        <h1>5</h1>
+        <div className="fruit-container">
+          <img
+            src={process.env.PUBLIC_URL + "/imgs/carrot.png"}
+            alt="Logo"
+            className="img-fruit"
+          />
+        </div>
+      </div>
+      <div className="broccoli">
+        <h1>Broccoli</h1>
+        <h1>1</h1>
+        <div className="fruit-container">
+          <img
+            src={process.env.PUBLIC_URL + "/imgs/brocoli.png"}
+            alt="Logo"
+            className="img-fruit"
+          />
+        </div>
+      </div>
+      <div className="orange">
+        <h1>Orange</h1>
+        <h1>0</h1>
+        <div className="fruit-container">
+          <img
+            src={process.env.PUBLIC_URL + "/imgs/orange.png"}
+            alt="Logo"
+            className="img-fruit"
+          />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
